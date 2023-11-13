4fc523cce13a935875a2c9fe33ff24e059004541
cyl3x <cyl3x@posteo.de>
2023-11-13 13:41:06 +0100

    Added cleanup on exit

--- bin/gen-iso.sh
+++ bin/gen-iso.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env -S bash -eo pipefail
 workdir="$(dirname "$0")/.."
 
-# /dev/disk/by-id/ata-Intenso_SSD_Sata_III_AYSNU0412OUT001587
+function cleanup {
+  rm -f "$workdir/post.sh" "$workdir/server.ign" "$workdir/server.bu" "$workdir/fcos-custom.iso"
+}
+trap cleanup EXIT
+
 flash_disk='/dev/disk/by-id/usb-USB_SanDisk_3.2Gen1_04019291b4b401a913da1b979ce4a7f3892c4631cb848f0ccde379e17b35055c7fe200000000000000000000f88b10a6ff9e0e188155810712ac409f-0:0'
 
 echo -e '#!/bin/bash\npoweroff' > "$workdir/post.sh"
@@ -10,15 +14,11 @@ echo -e '#!/bin/bash\npoweroff' > "$workdir/post.sh"
 
 rm -f "$workdir/fcos-custom.iso"
 
-
+# /dev/disk/by-id/ata-Intenso_SSD_Sata_III_AYSNU0412OUT001587
 coreos-installer iso customize \
     --dest-device /dev/disk/by-id/nvme-Samsung_SSD_950_PRO_512GB_S2GMNX0H918030M \
     --dest-ignition "$workdir/server.ign" \
     --post-install "$workdir/post.sh" \
     --output "$workdir/fcos-custom.iso" "$@"
 
-rm -f "$workdir/post.sh" "$workdir/server.ign" "$workdir/server.bu"
-
 sudo dd if="$workdir/fcos-custom.iso" of="$flash_disk" status=progress
-
-rm -f "$workdir/fcos-custom.iso"
